192a83cb6bfed3ca604d5a551a627221effa31a0
Apoorva1217 <Apoorva1217@gmail.com>
2020-07-15 23:08:08 +0530

    triplet combination and its percentage

--- Flip_Coin_Combination.sh
+++ Flip_Coin_Combination.sh
@@ -78,3 +78,86 @@ echo "TT percentage:"
 val=${dict[TT]}
 dict[TT]=`echo "scale=2;$val/$n*100" | bc`
 echo ${dict[TT]}
+
+dict[HHH]=0
+dict[HHT]=0
+dict[HTH]=0
+dict[THH]=0
+dict[TTH]=0
+dict[THT]=0
+dict[HTT]=0
+for((i=0;i<n;i++))
+do
+        r=$((RANDOM%8))
+        if [ $r -eq 0 ]
+        then
+                val=${dic[HHH]}
+                dict[HHH]=$((val+1))
+        elif [ $r -eq 1 ]
+        then
+                val=${dict[HHT]}
+                dict[HHT]=$((val+1))
+        elif [ $r -eq 2 ]
+        then
+                val=${dict[HTH]}
+                dict[HTH]=$((val+1))
+        elif [ $r -eq 3 ]
+        then
+		val=${dict[THH]}
+                dict[THH]=$((val+1))
+        elif [ $r -eq 4 ]
+        then
+                val=${dic[THH]}
+                dict[THH]=$((val+1))
+        elif [ $r -eq 5 ]
+        then
+                val=${dict[THT]}
+                dict[THT]=$((val+1))
+		elif [ $r -eq 6 ]
+        then
+                val=${dict[HTT]}
+                dict[HTT]=$((val+1))
+        else
+                val=${dict[TTT]}
+                dict[TTT]=$((val+1))
+        fi
+done
+echo "HHH percentage:"
+val=${dict[HHH]}
+dict[HHH]=`echo "scale=2;$val/$n*100" | bc`
+echo ${dict[HHH]}
+
+echo "HHT percentage:"
+val=${dict[HHT]}
+dict[HHT]=`echo "scale=2;$val/$n*100" | bc`
+echo ${dict[HHT]}
+
+echo "HTH percentage:"
+val=${dict[HTH]}
+dict[HTH]=`echo "scale=2;$val/$n*100" | bc`
+echo ${dict[HTH]}
+
+echo "THH percentage:"
+val=${dict[THH]}
+dict[THH]=`echo "scale=2;$val/$n*100" | bc`
+echo ${dict[THH]}
+
+echo "TTH percentage:"
+val=${dict[TTH]}
+dict[TTH]=`echo "scale=2;$val/$n*100" | bc`
+echo ${dict[TTH]}
+
+echo "THT percentage:"
+val=${dict[THT]}
+dict[THT]=`echo "scale=2;$val/$n*100" | bc`
+echo ${dict[THT]}
+
+echo "HTT percentage:"
+val=${dict[HTT]}
+dict[HTT]=`echo "scale=2;$val/$n*100" | bc`
+echo ${dict[HTT]}
+
+echo "TTT percentage:"
+val=${dict[TTT]}
+dict[TTT]=`echo "scale=2;$val/$n*100" | bc`
+echo ${dict[TTT]}
