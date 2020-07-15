318f6c22b3c0719c3a36597016c346b2a5b0be47
Apoorva1217 <Apoorva1217@gmail.com>
2020-07-15 23:04:19 +0530

    singlet combination and its percentage

--- Flip_Coin_Combination.sh
+++ Flip_Coin_Combination.sh
@@ -7,3 +7,30 @@ then
 else
         echo "Tail"
 fi
+echo "Enter number of flip:"
+read n
+
+declare -A dict
+
+dict[H]=0
+dict[T]=0
+for((i=0;i<n;i++))
+do
+        r=$((RANDOM%2))
+        if [ $r -eq 0 ]
+	then
+                val=${dic[H]}
+                dict[H]=$((val+1))
+        else
+                val=${dict[T]}
+                dict[T]=$((val+1))
+        fi
+done
+val=${dict[H]}
+echo "head percentage:"
+echo "scale=2;$val/$n*100" | bc
+
+val=${dict[T]}
+echo "tail percentage:"
+echo "scale=2;$val/$n*100" | bc
+
